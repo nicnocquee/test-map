55f4334dd68f33a5d64d1419343b90afe06a721b
Nico Prananta <nicnocquee@users.noreply.github.com>
2019-10-14 19:55:06 +0200

    Updates

---
+++ precache-manifest.1adfa91940c0a0c79f1c8ca3600b1ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253825c5fe9de3b2dbd449f647b527c8",
+    "url": "/test-map/index.html"
+  },
+  {
+    "revision": "1e70389082fd9fde5f3f",
+    "url": "/test-map/static/css/main.47ad8d59.chunk.css"
+  },
+  {
+    "revision": "adf4472d7d91edc9ee9b",
+    "url": "/test-map/static/js/2.e99c684e.chunk.js"
+  },
+  {
+    "revision": "1e70389082fd9fde5f3f",
+    "url": "/test-map/static/js/main.16240253.chunk.js"
+  },
+  {
+    "revision": "6ea08de3f4c4e632165f",
+    "url": "/test-map/static/js/runtime-main.8bbc724a.js"
+  }
+]);
